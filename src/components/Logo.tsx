84d7dee97f5dc544374f510e613c6806c4cc21c8
Elky Bachtiar <139325226+elky-bachtiar@users.noreply.github.com>
2025-06-04 12:22:56 +0200

    Updated Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export function Logo({ className = '' }: { className?: string }) {
   return (
     <Link to="/" className={`block ${className}`}>
-      <img src="/logo.svg" alt="Shop2Give" className="h-10" />
+      <img src="/logo.png" alt="Shop2Give" className="h-10" />
     </Link>
   );
 }
